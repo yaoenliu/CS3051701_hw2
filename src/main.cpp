963e21ebf2e3267edb2bcbd4f71530f4794b29be
Yaoen liu <peterliu1060@gmail.com>
2024-11-19 23:32:24 +0800

    fix bug and make LIST better

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,13 @@
 #include "Vector.h"
 #include "AdminId.h"
 
+// pin definitions
 #define LED_RED A4
 #define LED_GREEN A2
 #define RST_PIN 9
 #define SS_PIN 10
 
+// can hadle up to 100 authorized ID
 #define miximumAuthorizedID 100
 
 // task functions
@@ -19,7 +21,7 @@ void serialSection();
 String sRead(); // serial read
 String rRead(); // rfid read
 
-// authorized ID
+// authorized ID storage
 String authorizedID[miximumAuthorizedID];
 Vector<String> authorizedIDVector;
 
@@ -33,6 +35,7 @@ void setup()
   // initialize led pin
   pinMode(LED_RED, OUTPUT);
   pinMode(LED_GREEN, OUTPUT);
+  // set digital pins as ground
   pinMode(A5, OUTPUT);
   pinMode(A3, OUTPUT);
   digitalWrite(A5, LOW);
@@ -42,6 +45,7 @@ void setup()
   rfid.PCD_Init();
   // initialize authorized ID vector
   authorizedIDVector.setStorage(authorizedID);
+  // add the admin ID to the authorized ID list
   authorizedIDVector.push_back(adminID);
   // print system ready
   Serial.println("[System ready]");
@@ -56,30 +60,31 @@ void checkAuthorization()
 {
   while (1)
   {
-    Serial.println("[Place your card]");
+    Serial.println("[Place your card to authorize]");
     String input = rRead();
     bool authorized = false;
     for (uint16_t i = 0; i < authorizedIDVector.size(); i++)
     {
-      if (input == authorizedIDVector[i])
+      if (input == authorizedIDVector[i]) // check if the ID is in the authorized ID list
       {
         authorized = true;
         break;
       }
     }
-    if (authorized)
+    if (authorized) // if authorized, break the loop
     {
       break;
     }
-    else
+    else // if not authorized, blink the red led and retry
     {
-      Serial.println("[Access denied]");
+      Serial.println("[Access denied]\n");
       digitalWrite(LED_RED, HIGH);
       delay(1000);
       digitalWrite(LED_RED, LOW);
     }
   }
-  Serial.println("[Access granted]");
+  // if authorized, blink the green led and enter the admin mode
+  Serial.println("[Access granted]\n");
   digitalWrite(LED_GREEN, HIGH);
   delay(1000);
   digitalWrite(LED_GREEN, LOW);
@@ -89,23 +94,24 @@ void checkAuthorization()
 void serialSection()
 {
   Serial.println("[Enter admin mode]");
-  Serial.println("[Commands: ADD, LIST, REMOVE[index]]");
   while (1)
   {
-    String input = sRead();
+    Serial.println("[Commands: ADD, LIST, REMOVE[index]]");
+    String input = sRead(); // read command from serial
+
     if (input == "ADD")
     {
-      if (authorizedIDVector.full())
+      if (authorizedIDVector.full()) // check if the authorized ID list is full
       {
         Serial.println("[The authorized ID list is full]");
         break;
       }
-      Serial.println("[Place your card]");
+      Serial.println("[Place your card to add]");
       input = rRead();
       bool exists = false;
-      for (uint16_t i = 0; i < authorizedIDVector.size(); i++)
+      for (uint16_t i = 0; i < authorizedIDVector.size(); i++) // check if the ID already exists
       {
-        if (input == authorizedID[i])
+        if (input == authorizedID[i]) // if exists, break the loop
         {
           Serial.print("[ID: ");
           Serial.print(input);
@@ -114,41 +120,71 @@ void serialSection()
           break;
         }
       }
-      if (!exists)
+      if (!exists) // if not exists, add the ID to the authorized ID list
       {
         authorizedIDVector.push_back(input);
         Serial.print("[ID: ");
         Serial.print(input);
         Serial.println(" added]");
       }
-
+      Serial.println("");
       break;
     }
 
     if (input == "LIST")
     {
+      // draw the authorized ID list
+      Serial.println("   +----+---------------+");
+      Serial.println("   | No | Authorized ID |");
+
       for (uint16_t i = 0; i < authorizedIDVector.size(); i++)
       {
-        Serial.print(i + 1);
-        Serial.print(": ");
-        Serial.println(authorizedID[i]);
+        Serial.println("   +----+---------------+");
+        Serial.print("   | ");
+        if (i + 1 < 10)
+          Serial.print(" "); // fix the alignment
+        Serial.print(i + 1); // ID index
+        Serial.print(" | ");
+        for (int j = 0; j < 3; j++)
+          Serial.print(" ");
+        Serial.print(authorizedID[i]); // ID
+        for (int j = 0; j < 3; j++)
+          Serial.print(" ");
+        Serial.println("|");
       }
+      Serial.println("   +----+---------------+");
+      Serial.println("");
       break;
     }
 
     if (input.startsWith("REMOVE[") && input.endsWith("]"))
     {
-      if (authorizedIDVector.size() == 1)
+      if (authorizedIDVector.size() == 1) // check if the ID list has only the admin ID
       {
         Serial.println("[Cannot remove the only ID]");
         break;
       }
-      // remove the ID
+      // read the index from the input format
       int startIdx = input.indexOf('[') + 1;
       int endIdx = input.indexOf(']');
       String indexStr = input.substring(startIdx, endIdx);
+
+      // check if the index is a number
+      bool validIndex = true;
+      for (uint16_t i = 0; i < indexStr.length(); i++)
+      {
+        if (!isDigit(indexStr[i]))
+        {
+          Serial.print("[Invalid index, input: 1-");
+          Serial.print(authorizedIDVector.size());
+          Serial.println(" only]");
+          validIndex = false;
+          break;
+        }
+      }
+
       uint16_t index = indexStr.toInt();
-      if (index - 1 < authorizedIDVector.size() && index > 0)
+      if (index - 1 < authorizedIDVector.size() && index > 0 && validIndex) // check if the index is valid
       {
         Serial.print("[ID: ");
         Serial.print(authorizedID[index - 1]);
@@ -159,17 +195,21 @@ void serialSection()
       }
       else
       {
-        Serial.print("[Invalid index, input: 1 - ");
+        Serial.print("[Invalid index, input: 1-");
         Serial.print(authorizedIDVector.size());
         Serial.println(" only]");
       }
+      Serial.println("");
       break;
     }
+    // invalid command
+    Serial.println("[Invalid command]");
   }
 }
 
 String sRead()
 {
+  String input = "";
   // drop all serial input before reading
   while (Serial.available())
   {
@@ -179,7 +219,6 @@ String sRead()
   while (!Serial.available())
   {
   }
-  String input = "";
   input = Serial.readStringUntil('\n');
   input.trim();
   return input;
@@ -188,6 +227,7 @@ String sRead()
 String rRead()
 {
   String input = "";
+  // wait for card
   while (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial())
   {
   }
@@ -195,8 +235,8 @@ String rRead()
   {
     input += String(rfid.uid.uidByte[i], HEX);
   }
-  input.toUpperCase();
-  Serial.println("RFID read: " + input);
   rfid.PICC_HaltA();
+  input.toUpperCase();
+  Serial.println("[RFID read: " + input + "]");
   return input;
 }
